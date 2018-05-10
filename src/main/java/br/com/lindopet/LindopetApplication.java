c4e061342f367b2d98d340c12c863fcb05a3e21d
lindopet <lojalindopet@gmail.com>
2018-05-10 11:03:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/lindopet/LindopetApplication.java
@@ -0,0 +1,12 @@
+package br.com.lindopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindopetApplication.class, args);
+	}
+}
